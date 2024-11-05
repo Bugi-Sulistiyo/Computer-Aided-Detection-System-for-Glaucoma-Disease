f1030ae327acc424bd30bdce9c44be9dd2265302
Bugi-Sulistiyo <71893631+Bugi-Sulistiyo@users.noreply.github.com>
2024-11-05 17:41:27 +0800

    update the target dir path

--- scripts/1. Segmentation/a. dataset_download.py
+++ scripts/1. Segmentation/a. dataset_download.py
@@ -1,15 +1,16 @@
 # import packages
+## handling environment variables
 import os
 from dotenv import load_dotenv
-
+## download files from google drive
 from googledriver import download_folder
 
 # load environment variables
 load_dotenv()
 
 # variables for dataset download
-dataset_link = os.environ.get("DATASET_GDRIVE_LINK")
-folder_storage = "./dataset/"
+dataset_link = os.environ.get("DATASET_GDRIVE_LINK")    # google drive link for the dataset
+folder_storage = "./datasets/original_source"            # folder to store the downloaded dataset
 
 # download the dataset using the google drive link
 download_folder(dataset_link, folder_storage)
