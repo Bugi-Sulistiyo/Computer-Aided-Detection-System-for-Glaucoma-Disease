ccd9348bf3d5b13d1c0afd044897f8d171fe5a2e
bugi sulistiyo <bugisulistiyo@gmail.com>
2025-08-27 12:27:00 +0800

    implement path utilities on the script

--- scripts/2. Modelling/1. Data Preparation/c. filter_used_images.py
+++ scripts/2. Modelling/1. Data Preparation/c. filter_used_images.py
@@ -1,30 +1,23 @@
 # Import the needed package
-## package for handling files and directories
-import os
-import shutil
-## package for handling data
-import json
-import pandas as pd
-from dotenv import load_dotenv
-
+import sys                      # handling system arguments
+# add path to utilities directory
+sys.path.insert(0, "./../../utilities")
+import os                       # handling file operation basic
+import shutil                   # handling file operation for copying and removing
+import json                     # handling json file
+import pandas as pd             # handling dataframe for tabular data
+from dotenv import load_dotenv  # handling env variable file
+import paths                    # handling paths
+# importing the variable environment
 load_dotenv()
 
 # Global Variables
-path_full = os.environ.get("ORI_PATH")
-path_src_main = os.path.join(path_full, 'datasets', "preprocessed")
-path_src_fundus = os.path.join(path_src_main, 'fundus_image')
-path_src_mask = os.path.join(path_src_main, 'mask_image')
-path_dst_main = os.path.join(path_full, 'datasets', 'cleaned')
-path_dst_fundus = os.path.join(path_dst_main, 'fundus_image')
-path_dst_mask = os.path.join(path_dst_main, 'mask_image')
-path_annot = os.path.join(path_src_main, 'annotations')
-path_data = os.path.join(path_full, 'data')
-classes = os.listdir(path_src_fundus)
+classes = os.listdir(paths.path_prep_fundus)
 
 # Filter the used images
 ## import the metadata for each classes
-gcm_meta = json.load(open(os.path.join(path_annot, classes[0], 'annotations.json')))
-ngcm_meta = json.load(open(os.path.join(path_annot, classes[1], 'annotations.json')))
+gcm_meta = json.load(open(os.path.join(paths.path_prep_annot_target, classes[0], 'annotations.json')))
+ngcm_meta = json.load(open(os.path.join(paths.path_prep_annot_target, classes[1], 'annotations.json')))
 
 used_img_gcm = []
 used_img_ngcm = []
@@ -60,10 +53,10 @@ filter_result.loc[len(filter_result)] = ['total',
                                         round((len(used_img_gcm) + len(used_img_ngcm)) / (len(gcm_meta) + len(ngcm_meta)) * 100, 2),
                                         round((len(gcm_meta) + len(ngcm_meta) - len(used_img_gcm) - len(used_img_ngcm)) / (len(gcm_meta) + len(ngcm_meta)) * 100, 2)]
 ## save the dataframe
-filter_result.to_csv(os.path.join(path_data, 'filter_result.csv'), index=False)
+filter_result.to_csv(os.path.join(paths.path_docs, 'filter_result.csv'), index=False)
 
 # Create the destination directories
-for new_dir in [path_dst_fundus, path_dst_mask]:
+for new_dir in [paths.path_clean_fundus, paths.path_clean_mask]:
     if not os.path.exists(new_dir):
         os.makedirs(new_dir)
     else:
@@ -73,8 +66,8 @@ for new_dir in [path_dst_fundus, path_dst_mask]:
 for class_name, merged_img in {classes[0]:used_img_gcm, classes[1]:used_img_ngcm}.items():
     for img in merged_img:
         ## copy the fundus images
-        src_fundus_path = os.path.join(path_src_fundus, class_name, img)
-        dst_fundus_path = os.path.join(path_dst_fundus, img)
+        src_fundus_path = os.path.join(paths.path_prep_fundus, class_name, img)
+        dst_fundus_path = os.path.join(paths.path_clean_fundus, img)
         try:
             shutil.copy(src_fundus_path, dst_fundus_path)
         except FileNotFoundError:
@@ -82,8 +75,8 @@ for class_name, merged_img in {classes[0]:used_img_gcm, classes[1]:used_img_ngcm
         except shutil.Error:
             print(f'{dst_fundus_path} already exists')
         ## copy the mask images
-        src_mask_path = os.path.join(path_src_mask, class_name, img.replace('.jpg', '.png'))
-        dst_mask_path = os.path.join(path_dst_mask, img.replace('.jpg', '.png'))
+        src_mask_path = os.path.join(paths.path_prep_mask, class_name, img.replace('.jpg', '.png'))
+        dst_mask_path = os.path.join(paths.path_clean_mask, img.replace('.jpg', '.png'))
         try:
             shutil.copy(src_mask_path, dst_mask_path)
         except FileNotFoundError:
